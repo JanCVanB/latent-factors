755fe3d5df4007743af6f501d068c6662423f653
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-03-13 14:40:28 -0700

    Refactor svd_projection and add visualization module

--- python/svd_projection.py
+++ python/svd_projection.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 U_FILE_PATH = 'results/after_factorization/u.csv'
 V_FILE_PATH = 'results/after_factorization/v.csv'
 
@@ -9,30 +10,25 @@ def read_data():
     """
     u = np.genfromtxt(U_FILE_PATH, delimiter=',')
     v = np.genfromtxt(V_FILE_PATH, delimiter=',')
-   
     return u, v
 
 
-def project(dimensions, *matrices):
+def project(u, v, dimensions=2):
     """Factor all matrices with SVD and project them to the specified number of dimensions
 
+    :param np.array u: first matrix to project
+    :param np.array v: second matrix to project
     :param int dimensions: new dimensionality of the matrices
-    :param tuple matrices: matrices to project
     """
-    projected_matrices = []
-    for matrix in matrices:
-        a, _, _ = np.linalg.svd(matrix)
-        # TODO: determine if old code was correct (both av? transpose u?)
-        # u2 = np.dot(np.transpose(av[:, :2]), np.transpose(u))
-        # v2 = np.dot(np.transpose(av[:, :2]), v)
-        projected_matrix = np.dot(np.transpose(a[:, :dimensions]), matrix)
-        projected_matrices.append(projected_matrix)
-    return tuple(projected_matrices)
+    a, _, _ = np.linalg.svd(v)
+    u_new = np.dot(np.transpose(a[:, :dimensions]), np.transpose(u))
+    v_new = np.dot(np.transpose(a[:, :dimensions]), v)
+    return u_new, v_new
 
 
 def run():
     u, v = read_data()
-    u2, v2 = project(2, u, v)
+    u2, v2 = project(u, v)
     np.savetxt("results/after_svd/u_2dim.csv", u2, delimiter=',')
     np.savetxt("results/after_svd/v_2dim.csv", v2, delimiter=',')
 
